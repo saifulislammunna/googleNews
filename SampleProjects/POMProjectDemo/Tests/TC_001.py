51b8d526975ae32e1a0a71996489d35ea365da63
saifulislammunna <saifulmunna28@gmail.com>
2022-02-02 16:09:02 +0600

    TC_001 added

--- SampleProjects/POMProjectDemo/Tests/TC_001.py
+++ SampleProjects/POMProjectDemo/Tests/TC_001.py
@@ -4,6 +4,10 @@ from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path="E:\selenium-chrome-driver\chromedriver")
 
+
+driver.maximize_window()
 driver.get("https://news.google.com/topstories?hl=en-US&gl=US&ceid=US:en")
 
 time.sleep(2)
+
+driver.quit()
